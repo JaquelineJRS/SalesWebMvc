8ccca491670c7814fba5cc3e44a0b662b752b49a
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2022-05-25 18:30:54 -0300

    feat: criação do serviço SeedingService para popular o DB

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -5,10 +5,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 
@@ -40,14 +36,17 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                      builder.MigrationsAssembly("SalesWebMvc")));
 
+            services.AddScoped<SeedingService>();
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
