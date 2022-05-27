1178df31fc7ae21d9e371dc87516952907200478
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2022-05-26 21:45:52 -0300

    style: alteração na visualização de index no Seller

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -18,10 +18,11 @@ namespace SalesWebMvc.Models
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0}:dd/MM/yyyy")]
-        [Required(ErrorMessage = "{0} required")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "{0} required")]
